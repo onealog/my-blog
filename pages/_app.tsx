f6b93316a33b09c4f99da805054858fd1781eb3d
Onea <onea8906@gmail.com>
2022-02-15 00:47:57 +0900

    STYLE: globals.css를 styled-jsx로 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,31 @@
 import type { AppProps } from 'next/app';
 
-import '../styles/globals.css';
 import Layout from '../components/Layout';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
       <Component {...pageProps} />
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
     </Layout>
   );
 };
 
-export default MyApp
+export default MyApp;
