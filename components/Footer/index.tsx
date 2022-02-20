7d7467fd857b05e039179e59cdccc1ee5ca751e0
Onea <onea8906@gmail.com>
2022-02-21 01:14:48 +0900

    FEAT: Footer 컴포넌트 폴더화 및 Copyright 컴포넌트 구성

---
+++ components/Footer/index.tsx
@@ -0,0 +1,24 @@
+import Copyright from './Copyright';
+import Icon from './Icon';
+
+const Footer = () => {
+  return (
+    <footer>
+      <Copyright />
+      <Icon />
+      <style jsx>{`
+        footer {
+          display: flex;
+          flex: 1;
+          margin: 0 2rem;
+          justify-content: space-between;
+          align-items: center;
+          padding: 2rem 0;
+          border-top: 1px solid #eaeaea;
+        }
+        `}</style>
+    </footer>
+  );
+};
+
+export default Footer;
