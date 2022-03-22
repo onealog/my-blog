20886a7a58d43011eb0731fc006f768941fde728
onea <onea8906@gmail.com>
2022-03-22 23:53:00 +0900

    FEAT: PostHeader CSS 작업

--- src/components/Post/PostHeader.tsx
+++ src/components/Post/PostHeader.tsx
@@ -8,8 +8,49 @@ const PostHeader = ({ title, coverImage, date }: Props) => {
   return (
     <>
       <img src={coverImage} />
-      <div>{title}</div>
-      <div>{date}</div>
+      <div className="title-wrapper">
+        <h1>{title}</h1>
+        <div className="date">{date}</div>
+      </div>
+
+      <style jsx>{`
+        img {
+          object-fit: cover;
+          width: 100%;
+          height: 320px;
+        }
+
+        h1 {
+          font-size: 1.8rem;
+          margin-bottom: 0.5rem;
+        }
+
+        .title-wrapper {
+          display: flex;
+          flex-direction: column;
+          margin-left: auto;
+          margin-right: auto;
+          padding: 2rem;
+          min-width: 768px;
+        }
+
+        .date {
+          font-size: 1.2rem;
+          color: #7c7c7c;
+        }
+
+        @media (max-width: 600px) {
+          img {
+            height: 240px;
+          }
+        }
+
+        @media (max-width: 768px) {
+          .title-wrapper {
+            min-width: 100%;
+          }
+        }
+        `}</style>
     </>
   );
 };
