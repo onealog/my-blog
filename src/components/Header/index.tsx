20f4e9348613f03c1dbca5bd7fd4f3fba9bb9415
onealog <oneadev@gmail.com>
2023-01-16 04:05:47 +0900

    STYLE: header에 tailwind 적용

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,25 +3,9 @@ import Navbar from './Navbar';
 
 const Header = () => {
   return (
-    <header>
+    <header className="flex justify-between items-center mt-8">
       <Title />
       <Navbar />
-      <style jsx>{`
-        header {
-          display: flex;
-          justify-content: space-between;
-          align-items: center;
-          padding: 0 2rem;
-          margin-right: auto;
-          margin-left: auto;
-        }
-
-        @media (min-width: 768px) {
-          header {
-            max-width: 768px;
-          }
-        }
-      `}</style>
     </header>
   );
 };
