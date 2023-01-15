a50ed7030b234a6f163890c135fcfa0680ea7045
onealog <oneadev@gmail.com>
2023-01-16 04:05:13 +0900

    DELETE: _app에 스타일 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,46 +7,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
       <Component {...pageProps} />
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        hr {
-          border: 0 solid #eaeaea;
-          border-top-width: 1px;
-        }
-
-        ul {
-          margin: 0px;
-          padding: 0px;
-        }
-
-        li {
-          list-style: none;
-        }
-
-        ::selection {
-          background: #d7f7ff;;
-        }
-
-        ::-moz-selection {
-          background: #d7f7ff;;
-        }
-      `}</style>
     </Layout>
   );
 };
