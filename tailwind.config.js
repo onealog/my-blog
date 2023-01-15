ec2235e923c612568847224a5d4d65dbc63f738d
onealog <oneadev@gmail.com>
2023-01-16 03:10:14 +0900

    STYLE: tailwind 컬러 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/components/**/*.tsx', './src/pages/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          500: '#3C6255',
+          400: '#61876E',
+          300: '#A6BB8D',
+        },
+        gray: {
+          500: '#000000',
+          400: '#444444',
+          200: '#B8B8B8',
+          100: '#F2F2F2',
+          0: '#FFFFFF'
+        }
+      }
+    },
   },
   plugins: [],
 };
