b885ef4f24868a44a0dfff72e3f8b66df6a1b6d3
onealog <oneadev@gmail.com>
2023-01-18 01:46:38 +0900

    STYLE: Navbar 다크모드 아이콘 적용

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,25 +1,18 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { FaRegMoon } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="flex items-center text-xl">
       <Link href="/posts">
         <a>Posts</a>
       </Link>
       <Link href="/about">
-        <a>About</a>
+        <a className="ml-8">About</a>
       </Link>
-      <style jsx>{`
-        a {
-          margin-left: 0.8rem;
-          padding: 0.3rem 0.7rem;
-          border-radius: 50px;
-          font-size: 0.9rem;
-          font-weight: 600;
-          color: white;
-          background-color: #0b71cd;
-        }
-      `}</style>
+      <button className="ml-8 w-[35px] h-[35px] bg-gray-200 rounded">
+        <FaRegMoon className="text-sm my-0 mx-auto" />
+      </button>
     </nav>
   );
 };
