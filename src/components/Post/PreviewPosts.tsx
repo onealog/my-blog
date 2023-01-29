92cb1f88e34146ccce6301e534af5f0f735bf178
onealog <oneadev@gmail.com>
2023-01-30 00:18:07 +0900

    ADD: 포스트 레이아웃 작업 및 정적구조 완료

--- src/components/Post/PreviewPosts.tsx
+++ src/components/Post/PreviewPosts.tsx
@@ -1,36 +1,64 @@
+import { FaArrowRight } from "react-icons/fa";
 import Image from "next/image";
 import test from "./test.jpg";
 
 const PreviewPosts = () => {
   return (
-    <div className="mt-16 grid grid-cols-3 gap-5">
-      <a href="#" className="h-[340px] flex flex-col col-[1/3] row-[1/3]">
+    <div className="mt-16 grid gap-5 grid-cols-2">
+      <a
+        href="#"
+        className="h-[340px] flex flex-col col-[1/3] transition hover:text-green-400 hover:scale-[101%]"
+      >
         <Image
           src={test}
-          alt="d"
+          alt="test"
           width={100}
-          sizes="100vw"
-          // layout="fill"
           objectFit="cover"
+          className="rounded"
         />
+        <h1 className="mt-2 text-xl font-bold">블로그를 시작하며...</h1>
+        <p className="text-xs font-semibold text-gray-300">
+          2023년 1월 13일 • 읽는 데 4분
+        </p>
       </a>
-      <a href="#" className="flex flex-col">
+      <a
+        href="#"
+        className="flex flex-col transition hover:text-green-400 hover:scale-[101%]"
+      >
         <Image
-          src="/public/onea.jpeg"
-          alt=""
+          src={test}
+          alt="test"
           width={100}
-          height={300}
-          className="bg-green-500 object-cover"
+          height={220}
+          className="bg-green-500 object-cover rounded"
         />
+        <h1 className="mt-2 text-xl font-bold">블로그를 시작하며...</h1>
+        <p className="text-xs font-semibold text-gray-300">
+          2023년 1월 13일 • 읽는 데 4분
+        </p>
       </a>
-      <a href="#" className="flex flex-col">
+      <a
+        href="#"
+        className="flex flex-col transition hover:text-green-400 hover:scale-[101%]"
+      >
         <Image
-          src="/public/onea.jpeg"
-          alt=""
+          src={test}
+          alt="test"
           width={100}
-          height={300}
-          className="bg-gray-200 object-cover"
+          height={220}
+          className="bg-gray-200 object-cover rounded"
         />
+        <h1 className="mt-2 text-xl font-bold">블로그를 시작하며...</h1>
+        <p className="text-xs font-semibold text-gray-300">
+          2023년 1월 13일 • 읽는 데 4분
+        </p>
+      </a>
+      <a
+        href="#"
+        className="flex items-center mt-3 text-gray-300 font-semibold text-xs hover:text-green-400"
+      >
+        모든 글 보러 가기
+        <FaArrowRight className="ml-1" />
       </a>
     </div>
   );
