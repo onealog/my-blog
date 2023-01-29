a196246c92a39054b6afa54aefaf1ec9a0c10b35
onealog <oneadev@gmail.com>
2023-01-30 01:05:31 +0900

    STYLE: 드래그 색상 변경

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 
 const Layout: FC = ({ children }) => {
   return (
-    <div className="md:w-[768px] my-0 mx-auto px-8">
+    <div className="md:w-[768px] my-0 mx-auto px-8 selection:text-white selection:bg-green-400">
       <Header />
       <div>{children}</div>
       <Footer />
