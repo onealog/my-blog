9e1fa7878297caf2c638f9704bfa0af4dfb86677
onealog <oneadev@gmail.com>
2023-01-30 01:15:23 +0900

    STYLE: Navbar 스타일 수정

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -5,12 +5,16 @@ const Navbar = () => {
   return (
     <nav className="flex items-center text-xl">
       <Link href="/posts">
-        <a>Posts</a>
+        <a className="hover:scale-[105%] hover:text-green-500 transition">
+          Posts
+        </a>
       </Link>
       <Link href="/about">
-        <a className="ml-8">About</a>
+        <a className="ml-8 hover:scale-[105%] hover:text-green-500 transition">
+          About
+        </a>
       </Link>
-      <button className="ml-8 w-[35px] h-[35px] bg-gray-200 rounded">
+      <button className="ml-8 w-[35px] h-[35px] bg-gray-200 rounded transition hover:border-gray-300 hover:border-2">
         <FaRegMoon className="text-sm my-0 mx-auto" />
       </button>
     </nav>
