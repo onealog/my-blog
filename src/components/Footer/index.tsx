8c192e8feec72a48820879558e2ad228673f6b11
onealog <oneadev@gmail.com>
2023-01-30 00:48:10 +0900

    UPDATE: Footer 수정 작업

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,29 +1,58 @@
-import Copyright from "./Copyright";
-import Icon from "./Icon";
+import {
+  BsFacebook,
+  BsGithub,
+  BsLinkedin,
+  BsFileEarmarkText,
+} from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <footer>
-      <Copyright />
-      <Icon />
-      <style jsx>{`
-        footer {
-          display: flex;
-          margin: 0.5rem 2rem;
-          justify-content: space-between;
-          align-items: center;
-          border-top: 1px solid #eaeaea;
-          padding: 2rem;
-          margin-right: auto;
-          margin-left: auto;
-        }
-
-        @media (min-width: 768px) {
-          footer {
-            max-width: 768px;
-          }
-        }
-      `}</style>
+    <footer className="flex justify-between my-16 border-t pt-8">
+      <div className="text-gray-300 text-xs font-semibold">
+        <span>copyrightⓒ 2023 All rights reserved by </span>
+        <a
+          href="mailto:oneadev@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline"
+        >
+          ONE:A.
+        </a>
+      </div>
+      <div className="flex">
+        <a
+          href="https://github.com/onealog"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mr-3 text-2xl text-gray-300 hover:text-gray-400"
+        >
+          <BsGithub />
+        </a>
+        <a
+          href="https://www.facebook.com/artistonea/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mr-3 text-2xl text-gray-300 hover:text-gray-400"
+        >
+          <BsFacebook />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jaewon-han/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mr-3 text-2xl text-gray-300 hover:text-gray-400"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://brunch.co.kr/@onea"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mr-3 text-2xl text-gray-300 hover:text-gray-400"
+        >
+          <BsFileEarmarkText />
+        </a>
+      </div>
     </footer>
   );
 };
