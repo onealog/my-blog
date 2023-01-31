807762296f771223da9e9cc3c864d705c891c35f
onealog <oneadev@gmail.com>
2023-02-01 02:26:01 +0900

    CHORE: 안쓰는 코드나 불필요한 코드 수정

--- src/components/About/CardListWithLink.tsx
+++ src/components/About/CardListWithLink.tsx
@@ -7,24 +7,22 @@ const CardListWithLink = ({
   description,
 }: CardListWithLinkProps) => {
   return (
-    <>
-      <div className="flex items-center mb-3">
-        <div className="name-link">
-          <div className="w-[200px]">
-            <a
-              href={link}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="font-bold text-lg mb-0 text-gray-400 hover:text-green-300"
-            >
-              {title}
-            </a>
-          </div>
-          <h4 className="text-xs mt-0 text-gray-300">{date}</h4>
+    <div className="flex items-center mb-3">
+      <div className="name-link">
+        <div className="w-[200px]">
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-bold text-lg mb-0 text-gray-400 hover:text-green-300"
+          >
+            {title}
+          </a>
         </div>
-        <p className="text-sm text-gray-400 pl-4">{description}</p>
+        <h4 className="text-xs mt-0 text-gray-300">{date}</h4>
       </div>
-    </>
+      <p className="text-sm text-gray-400 pl-4">{description}</p>
+    </div>
   );
 };
 
