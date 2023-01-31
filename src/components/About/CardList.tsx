f28c8a540b174ab10f7e16f9383dfbb4cc5578bd
onealog <oneadev@gmail.com>
2023-02-01 02:29:50 +0900

    UPDATE: 태그 정리

--- src/components/About/CardList.tsx
+++ src/components/About/CardList.tsx
@@ -3,8 +3,8 @@ import { CardListProps } from "../../types/styles";
 const CardList = ({ title, date, description }: CardListProps) => {
   return (
     <div className="flex items-center mb-3">
-      <div>
-        <h3 className="font-bold text-lg mb-0 w-[200px] text-gray-400">{title}</h3>
+      <div className="w-[200px]">
+        <h3 className="font-bold text-lg mb-0 text-gray-400">{title}</h3>
         <h4 className="text-xs mt-0 text-gray-300">{date}</h4>
       </div>
       <p className="text-sm text-gray-400 pl-4">{description}</p>
